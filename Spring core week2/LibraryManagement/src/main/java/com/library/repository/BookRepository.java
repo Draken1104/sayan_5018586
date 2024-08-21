f06373d4be013d061fb70c0f96d4ba77fc8cb9d6
Sayan Ghosh <122163826+Draken1104@users.noreply.github.com>
2024-08-21 14:40:40 +0530

    Add files via upload

---
+++ Spring core week2/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void addBook() {
+        // Implement your data access logic here
+        System.out.println("Adding a book to the repository");
+    }
+}
---
+++ Spring core week2/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void manageBooks() {
+        // Implement your business logic here
+        System.out.println("Managing books in the library");
+    }
+}
+
